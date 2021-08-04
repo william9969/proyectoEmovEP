f8628cfd1cddc1bb60f890acdfd9d648d9434483
william9969 <wsinchi69@gmail.com>
2021-08-04 10:26:36 -0500

    Dependencias con Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDLdlve_NVivfGxxIeDLgf6K0Pjio0bDk4",
+    authDomain: "proyecto-final-9bfd7.firebaseapp.com",
+    projectId: "proyecto-final-9bfd7",
+    storageBucket: "proyecto-final-9bfd7.appspot.com",
+    messagingSenderId: "594407734453",
+    appId: "1:594407734453:web:7011628bea8601a5c1d2fc"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyDLdlve_NVivfGxxIeDLgf6K0Pjio0bDk4",
+    authDomain: "proyecto-final-9bfd7.firebaseapp.com",
+    projectId: "proyecto-final-9bfd7",
+    storageBucket: "proyecto-final-9bfd7.appspot.com",
+    messagingSenderId: "594407734453",
+    appId: "1:594407734453:web:7011628bea8601a5c1d2fc"
+  }
+
 };
 
 /*
