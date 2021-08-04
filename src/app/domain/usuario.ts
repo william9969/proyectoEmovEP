ccefc4070021a8eaeaf387dea66c030a23d7e417
Pedro Orellana <porellanaj@est.ups.edu.ec>
2021-08-04 16:01:11 -0500

    Actualizacon pagina login

---
+++ src/app/domain/usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+    uid: string;
+    nombre: string;
+    apellido: string;
+    correo: string;
+    clave: string;
+    telefono: string;
+    rol: string;
+}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../domain/usuario';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario();
+  usuario2: Usuario = new Usuario();
+
+  constructor(private usuarioService: UsuariosService,
+    private route:Router) { }
 
   ngOnInit() {
   }
 
+  async login(){
+    const correo = this.usuario.correo
+    const clave = this.usuario.clave
+    this.usuarioService.findUser(correo, clave).subscribe(data=>{
+      console.log(data[0])
+      this.usuario2=data[0]
+      try{
+        if(this.usuario2.correo==correo && this.usuario2.clave==clave && this.usuario2.rol=='agente'){
+          this.route.navigate(['agente']);
+        }else if(this.usuario2.correo==correo && this.usuario2.clave==clave && this.usuario2.rol=='cliente'){
+          this.route.navigate(['cliente']);
+        }
+      }
+      catch(error){console.log('Error: ->', error);
+        this.route.navigate(['login']);}
+    });
+  }
 }
