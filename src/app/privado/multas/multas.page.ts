57ed1cea88dc8fd8216c67376313091e1bfca12f
william9969 <wsinchi69@gmail.com>
2021-08-07 14:05:45 -0500

    Implementacion de la Geolocalizacion

--- src/app/privado/multas/multas.page.ts
+++ src/app/privado/multas/multas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-multas',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultasPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  public menuAgente = [
+    { icon: 'home-outline', nombre: 'Inicio',path:'privado/principalAgentes'},
+    { icon: 'clipboard-outline', nombre: 'Crear Multas',path:'privado/multas'},
+    { icon: 'business-outline', nombre: 'Agencias EmovEP',path:'privado/agencias'},
+    { icon: 'people-outline', nombre: 'Acerca de',path:'privado/acerca-de'},
+    { icon: 'mail-outline', nombre: 'Contactenos',path:'privado/contactenos'},
+  ];
+  navegar(nombre: any){
+    //  console.log(nombre)
+      this.router.navigate([nombre])
+  }
 }
